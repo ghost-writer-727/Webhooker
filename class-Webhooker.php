d64531d896552d18c83e9e021334cba578073ec6
ghost-writer-727 <4065416+ghost-writer-727@users.noreply.github.com>
2023-08-02 12:02:58 -0400

    Add Timeouts to kill hung requests

--- class-Webhooker.php
+++ class-Webhooker.php
@@ -11,9 +11,10 @@ class Webhooker {
 	 * Send data to Zapier
 	 * @param string $webhookUrl The webhook URL
 	 * @param array $data The data to send
+	 * @param int $timeout The timeout in seconds
 	 * @return array
 	 */
-	public static function send($webhookUrl, $data = []) {
+	public static function send($webhookUrl, $data = [], $timeout = 10) {
 		// Validate the webhook URL
         if (!filter_var($webhookUrl, FILTER_VALIDATE_URL)) {
             throw new InvalidArgumentException('Invalid webhook URL.');
@@ -23,13 +24,21 @@ class Webhooker {
         if (!is_array($data) && !is_string($data)) {
             throw new InvalidArgumentException('Data must be an array or a string.');
         }
-		
+
+		// Validate that timeout is an integer
+		if (!is_int($timeout)) {
+			throw new InvalidArgumentException('Timeout must be an integer.');
+		}
+
 		$ch = curl_init($webhookUrl);
+		$timeout = 10; // Seconds
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-		
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+
 		$response = curl_exec($ch);
 
 		if ($response === false) {

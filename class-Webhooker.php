a0ca8a9aabeda871e719cafb63038e1cdbdea302
ghost-writer-727 <4065416+ghost-writer-727@users.noreply.github.com>
2023-08-02 10:50:23 -0400

    Initial commit

---
+++ class-Webhooker.php
@@ -0,0 +1,45 @@
+<?php defined( 'ABSPATH' ) ||  exit; // Exit if accessed directly
+/** 
+ * Webhooker
+ * @version 1.0.0
+ * A simple class to simplify sending data to Zapier and other webhook services.
+ */
+
+if( ! class_exists( 'Webhooker' ) ):
+class Webhooker {
+	/**
+	 * Send data to Zapier
+	 * @param string $webhookUrl The webhook URL
+	 * @param array $data The data to send
+	 * @return array
+	 */
+	public static function send($webhookUrl, $data = []) {
+		// Validate the webhook URL
+        if (!filter_var($webhookUrl, FILTER_VALIDATE_URL)) {
+            throw new InvalidArgumentException('Invalid webhook URL.');
+        }
+
+        // Validate the data
+        if (!is_array($data) && !is_string($data)) {
+            throw new InvalidArgumentException('Data must be an array or a string.');
+        }
+		
+		$ch = curl_init($webhookUrl);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		curl_setopt($ch, CURLOPT_POST, 1);
+
+		$response = curl_exec($ch);
+		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		curl_close($ch);
+
+		return [
+			'success' => $httpCode == 200,
+			'response' => $response,
+			'http_code' => $httpCode
+		];
+	}
+}
+
+endif;

3871af30c70a2600b49c000bc171ebf57f9a74c0
ghost-writer-727 <4065416+ghost-writer-727@users.noreply.github.com>
2023-08-02 11:50:40 -0400

    Explicit Content-Type Header for compatability

--- class-Webhooker.php
+++ class-Webhooker.php
@@ -28,7 +28,8 @@ class Webhooker {
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 		curl_setopt($ch, CURLOPT_POST, 1);
-
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+		
 		$response = curl_exec($ch);
 
 		if ($response === false) {
